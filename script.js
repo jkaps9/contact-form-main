71b56145cd6f2ea8790e6d8bda8d21e55c4082e0
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-04 00:36:58 -0400

    show message on form submit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const form = document.getElementById("contact-form");
+const successMessage = document.querySelector(".success-message");
+
+// Form submission handler
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  successMessage.classList.add("visible");
+  form.reset();
+  setTimeout(() => successMessage.classList.remove("visible"), 5000);
+});
